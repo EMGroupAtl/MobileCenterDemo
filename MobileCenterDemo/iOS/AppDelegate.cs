a72e121de6cf28ce876fa4a11808fbcc37062e89
Aranha, Vivian <vivian.aranha@delta.com>
2017-09-07 18:17:53 -0400

    Tests

--- MobileCenterDemo/iOS/AppDelegate.cs
+++ MobileCenterDemo/iOS/AppDelegate.cs
@@ -12,7 +12,13 @@ namespace MobileCenterDemo.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+
+            #if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+            #endif
+
+
+			global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
 
